f5c424cd994efe17016532ca0f8b3f25f04728a7
Adnan Duric <aduric@gmail.com>
2016-11-12 21:05:46 -0500

    Fixes test

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -82,7 +82,8 @@ impl Tokenizer {
         pub fn tokenize(&mut self, token_str: &Vec<u8>, state_map: &str, token_map: &str) -> () {
                 let mut curr_token: &mut Vec<u8> = &mut Vec::new();
                 let mut raw_bytes = token_str.clone();
-                let mut curr_state = State(0);
+                let start_state = State(utils::get_hash_val(&String::from("Start").into_bytes()));
+                let mut curr_state = start_state;
 
                 let s_map: HashMap<State, HashMap<TokenType, State>> = Tokenizer::compile_states(state_map);
                 let t_map: HashMap<u8, TokenType> = Tokenizer::compile_tokens_ascii(token_map);
@@ -100,10 +101,12 @@ impl Tokenizer {
                         };
 
                         let new_state = match s_map.get(&curr_state).unwrap().get(&curr_byte_t) {
-                                None => State(0),
+                                None => State(utils::get_hash_val(&String::from("Start").into_bytes())),
                                 Some(v) => {
-                                        curr_token.push(raw_bytes.remove(0));
-                                        if v != &curr_state {
+                                        if State(utils::get_hash_val(&String::from("Start").into_bytes())) != curr_state {
+                                                curr_token.push(raw_bytes.remove(0));
+                                        }
+                                        if v != &curr_state && curr_state != State(utils::get_hash_val(&String::from("Start").into_bytes())) {
                                                 self._emit_token(Token { t: curr_byte_t.to_owned(), value: curr_token.to_owned()} );
                                                 curr_token.clear();
                                         };
@@ -185,6 +188,10 @@ mod tests {
 
                 let transitions =
                 "
+                Start => Alpha => Alpha
+                Start => Number => Number
+                Start => Whitespace => Whitespace
+                Start => Punctuation => Punctuation
                 Alpha => Alpha | Number => Alpha
                 Slash => Slash => Pos
                 Pos => Alpha => Pos
