68fbb498e0e09d81927f85e78fdf2e38ac2b21cf
aduric <adnan.duric@d2l.com>
2016-12-28 10:07:13 -0500

    Adds lev distance for real

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
 pub mod utils {
+
+    use std::cmp::min;
+
     pub fn get_hash_val(token: &Vec<u8>) -> u32 {
         let mut hash_val: u32 = 0;
         for i in 0..token.len() {
@@ -6,8 +9,43 @@ pub mod utils {
         };
         hash_val
     }
+    pub fn levenshtein(a: &Vec<u8>, b: &Vec<u8>) -> u32 {
+        let a_len = a.len();
+        let b_len = b.len();
+        let cap = a_len * b_len;
+        let mut lev: Vec<u32> = Vec::with_capacity(cap);
 
-    pub fn levenshtein_distance(word1: &Vec<u8>, word2: &Vec<u8>) -> u32 {
-        0
+        for j in 0..b_len {
+            lev[j] = j as u32;
+        };
+
+        for i in 0..a_len {
+            lev[i*a_len] = i as u32;
+            for j in 0..b_len {
+                if a[i*a_len] == b[i*a_len+j] {
+                    lev[i*a_len+j] = min(lev[i*a_len+j-1] + 1, min(lev[i*(a_len-1)+j] + 1, lev[i*(a_len-1)+j-1]));
+                } else {
+                    lev[i*a_len+j] = min(lev[i*a_len+j-1] + 1, min(lev[i*(a_len-1)+j] + 1, lev[i*(a_len-1)+j-1] + 1));
+                }
+            }
+        };
+        lev[(a_len-1)*(b_len-1)-1]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::str;
+
+    #[test]
+    fn get_levenshtein_distance() {
+        let a_vec: Vec<u8> = String::from("algorithm").into_bytes();
+        let b_vec: Vec<u8> = String::from("altruistic").into_bytes();
+
+        let lev = utils::levenshtein(&a_vec, &b_vec);
+
+        assert!(lev == 6);
     }
 }
