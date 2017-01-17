23f4ef71913bbcb8a87f9e8d89361fe013b011fb
aduric <adnan.duric@d2l.com>
2017-01-17 17:17:45 -0500

    Can build corpus from brown corpus

--- src/corpus.rs
+++ src/corpus.rs
@@ -13,13 +13,56 @@ pub struct Corpus {
 
 impl Corpus {
     pub fn new(root_path: &str, tokenizer: Tokenizer) -> Self {
-        let paths = fs::read_dir(root_path).unwrap().map(|f| f.unwrap().file_name().into_string().unwrap()).collect::<Vec<String>>();
-        let scanners: Vec<Scanner> = paths.iter().map(|f| Scanner::new(&format!("{}{}", root_path, f))).collect::<Vec<Scanner>>();
+        let scanners: Vec<Scanner> = Corpus::register_scanners(root_path);
 
         Corpus {
             scanners: scanners,
             tokenizer: tokenizer
         }
+    }
+    pub fn brown() -> Self {
+        let tokens: &'static str = 
+            "
+            Alpha => 65..123
+            Number => 48..57
+            Whitespace => 9,10,13,32
+            Punctuation => 33..46
+            Punctuation => 58..65
+            Slash => 47
+            ";
+
+        let transitions: &'static str = 
+            "
+            Start => Alpha => Alpha
+            Start => Number => Number
+            Start => Whitespace => Whitespace
+            Start => Punctuation => Punctuation
+            Start => Slash => Slash
+            Slash => Slash => Slash
+            Slash => Whitespace => Whitespace
+            Slash => Alpha => Pos
+            Alpha => Alpha | Number => Alpha
+            Pos => Alpha => Pos
+            Number => Number => Number
+            Number => Alpha => Alpha
+            Whitespace => Whitespace => Whitespace
+            Punctuation => Punctuation => Punctuation
+            ";
+
+        let tokenizer = Tokenizer::new(&tokens, &transitions);
+        let brown_path: &'static str = "/brown/";
+
+        let scanners: Vec<Scanner> = Corpus::register_scanners(brown_path);
+
+        Corpus {
+            scanners: scanners,
+            tokenizer: tokenizer
+        }
+     
+    }
+    fn register_scanners(root_path: &str) -> Vec<Scanner> {
+        let paths = fs::read_dir(root_path).unwrap().map(|f| f.unwrap().file_name().into_string().unwrap()).collect::<Vec<String>>();
+        paths.iter().map(|f| Scanner::new(&format!("{}{}", root_path, f))).collect::<Vec<Scanner>>()
     }
     pub fn get_scanners(&self) -> &Vec<Scanner> {
         &self.scanners
